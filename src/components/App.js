202d19a6d653a1034ec633864aef5209901d4f73
Fredyspt <fredyfps@gmail.com>
2021-07-29 17:16:38 -0600

    Created badge details page and a modal to delete badges
    Signed-off-by: Fredyspt <fredyfps@gmail.com>

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ import BadgeNew from "../pages/BadgeNew";
 import Badges from "../pages/Badges";
 import NotFound from '../pages/NotFound'
 import BadgeEdit from '../pages/BadgeEdit';
-import BadgeDetails from '../pages/BadgeDetails';
+import BadgeDetailsContainer from '../pages/BadgeDetailsContainer';
 
 function App() {
   return (
@@ -18,7 +18,7 @@ function App() {
           <Route exact path="/badges" component={Badges} />
           <Route exact path="/badges/new" component={BadgeNew} />
           <Route exact path="/badges/:badgeId/edit" component={BadgeEdit} />
-          <Route exact path="/badges/:badgeId" component={BadgeDetails} />
+          <Route exact path="/badges/:badgeId" component={BadgeDetailsContainer} />
           <Route component={NotFound} />
         </Switch>
       </Layout>
--- src/pages/BadgeDetails.js
+++ src/pages/BadgeDetails.js
@@ -1,97 +1,71 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 
-import PageLoading from "../components/PageLoading";
-import PageError from "../components/PageError";
 import Badge from "../components/Badge";
+import DeleteBadgeModal from "../components/DeleteBadgeModal";
 
 import "./styles/BadgeDetails.css";
 import confLogo from "../images/platziconf-logo.svg";
-import api from "../api";
 
-class BadgeDetails extends Component {
-  state = {
-    loading: true,
-    error: null,
-    data: undefined,
-  };
+function BadgeDetails(props) {
+  const badge = props.badge;
 
-  componentDidMount() {
-    this.fetchData();
-  }
-
-  fetchData = async (e) => {
-    this.setState({ loading: true, error: null });
-
-    try {
-      const data = await api.badges.read(this.props.match.params.badgeId);
-
-      this.setState({ loading: false, data: data });
-    } catch (error) {
-      this.setState({ loading: false, error: error });
-    }
-  };
-
-  render() {
-    if (this.state.loading) {
-      return <PageLoading />;
-    }
-
-    if (this.state.error) {
-      return <PageError error={this.state.error} />;
-    }
-
-    const badge = this.state.data;
-    return (
-      <React.Fragment>
-        <div className="BadgeDetails__hero">
-          <div className="container">
-            <div className="row">
-              <div className="col-6">
-                <img src={confLogo} alt="Conf logo" />
-              </div>
-              <div className="col-6 BadgeDetails__hero-attendant-name">
-                <h1>
-                  {badge.firstName} {badge.lastName}
-                </h1>
-              </div>
-            </div>
-          </div>
-        </div>
-
+  return (
+    <React.Fragment>
+      <div className="BadgeDetails__hero">
         <div className="container">
           <div className="row">
-            <div className="col">
-              <Badge
-                firstName={badge.firstName}
-                lastName={badge.lastName}
-                twitter={badge.twitter}
-                jobTitle={badge.jobTitle}
-                email={badge.email}
-              />
+            <div className="col-6">
+              <img src={confLogo} alt="Conf logo" />
             </div>
-            <div className="col">
-              <h2>Actions</h2>
-              <div>
-                <div>
-                  <Link
-                    className="btn btn-primary mb-4"
-                    to={`/badges/${badge.id}/edit`}
-                  >
-                    Edit
-                  </Link>
-                </div>
-              </div>
-
-              <div>
-                <button className="btn btn-danger">Delete</button>
-              </div>
+            <div className="col-6 BadgeDetails__hero-attendant-name">
+              <h1>
+                {badge.firstName} {badge.lastName}
+              </h1>
             </div>
           </div>
         </div>
-      </React.Fragment>
-    );
-  }
+      </div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <Badge
+              firstName={badge.firstName}
+              lastName={badge.lastName}
+              twitter={badge.twitter}
+              jobTitle={badge.jobTitle}
+              email={badge.email}
+            />
+          </div>
+          <div className="col">
+            <h2>Actions</h2>
+            <div>
+              <div>
+                <Link
+                  className="btn btn-primary mb-4"
+                  to={`/badges/${badge.id}/edit`}
+                >
+                  Edit
+                </Link>
+              </div>
+            </div>
+
+            <div>
+              <button onClick={props.onOpenModal} className="btn btn-danger">
+                Delete
+              </button>
+              <DeleteBadgeModal
+                isOpen={props.modalIsOpen}
+                onClose={props.onCloseModal}
+                onDeleteBadge={props.onDeleteBadge}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default BadgeDetails;
